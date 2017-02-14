b804e8936ff8ceff4fee763dce17ec06329ddac5
Zentarg <apg.chili@gmail.com>
2017-02-14 13:42:00 +0100

    Finished animations on search screen, aswell as started on the functionality, giving and removing attributes when a result is clicked

--- www/js/jquery.js
+++ www/js/jquery.js
@@ -1,17 +1,56 @@
 $(document).ready(function() {
     console.log("Jquery Loaded");
     
-    $gymSearch = $('#GymSearch');
-    $gymSearchVal = $('#GymSearch').val().length;
+    $entrySearch = $('#entrySearch');
+    $results = $('.results');
+    $resultsContainer = $('.resultsContainer');
+    $searchType = "Gym";
     
-   $($gymSearch).keyup(function() {
-       console.log($gymSearchVal);
-       console.log($('#GymSearch').val());
-      if ($gymSearchVal >= 1) {
-          $($gymSearch).css("margin-top", "2em");
-      } else {
-          $($gymSearch).css("margin-top", "16em");
+    $($entrySearch).keyup(function() {
+       $entrySearchVal = $('#entrySearch').val();
+       $entrySearchValLength = String($('#entrySearch').val()).length;
+       
+       if ($entrySearchValLength >= 1) {
+           $($entrySearch).css("margin-top", "15%");
+           $($resultsContainer).css("opacity", "1");
+           $($resultsContainer).css("margin-top", "0%");
+           $($results).css("margin-top", "2%");
+       } else {
+           $($entrySearch).css("margin-top", "55%");
+           $($resultsContainer).css("opacity", "0");
+           $($resultsContainer).css("margin-top", "50%");
+           $($results).css("margin-top", "20%");
+           
       };
-   });
+    });
+    
+    $($results).click(function() {
+        $searchType = "Student";
+        
+        console.log($(this).text());
+        console.log($(this).attr("gymID"));
+        
+        $($resultsContainer).css("margin-top", "50%");
+        $($resultsContainer).css("opacity", "0");
+        $($results).css("margin-top", "20%");
+        
+        $($entrySearch).attr("placeholder", "Søg Elev");
+        $($results).removeAttr("gymID");
+        
+        $studentIDs = [
+            1,
+            2,
+            3,
+            4
+        ];
+        $i = 0;
+        $($results).each(function(index) {
+            
+            $(this).attr("studentID", $studentIDs[$i])
+            $i++;
+            console.log($i);
+        });
+        
+    });
     
 });
