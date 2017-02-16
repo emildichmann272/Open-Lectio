0712b084a3cbaf5150de5aed2faa42d182779b88
Emil Dichmann <emil@dichmann.info>
2017-02-16 21:44:35 +0100

    added a function that returns the names of all students from a gym. right now the function is not working due to the request running async on the same thread which is not allowed

--- www/js/Download.js
+++ www/js/Download.js
@@ -1,15 +1,16 @@
-var xhttp = new XMLHttpRequest();
 var list = [];
-var IDs = [];
 var loaded = false;
-
+var alphabet = "abcdefghijklmnopqrstuvwxyzæøå";
+var IDs = [];
 function download(url) {
+    var xhttp = new XMLHttpRequest();
     xhttp.open("GET", url, true);
     xhttp.send();
+    return xhttp;
 }
 
 function gymlist() {
-    var wtf = new download("http://www.lectio.dk/lectio/login_list.aspx");
+    var xhttp = new download("http://www.lectio.dk/lectio/login_list.aspx");
     xhttp.onreadystatechange = function () {
     	if (this.readyState === 4 && this.status === 200) {
         var html = $.parseHTML(this.responseText);
@@ -18,8 +19,27 @@ function gymlist() {
 			list.push(gym.getAttribute("href").replace("/lectio/", "").replace("/default.aspx", "")+"=="+gym.text);
 		})
 		loaded = true;
+        namelist("523");
 		};
     }
+}
+
+function namelist(ID) {
+    list = [];
+    var requests = [];
+    var letters = alphabet.split("");
+    letters.forEach(function(item, index) {
+        requests.push(new download("https://www.lectio.dk/lectio/"+ID+"/FindSkema.aspx?type=elev&forbogstav="+item))
+        requests[index].onreadystatechange = function () {
+            if (this.readyState === 4 && this.status === 200) {
+                var html = $.parseHTML(this.responseText);
+		        var gyms = $(html).find("a");
+		        $(gyms).each(function(i, gym){
+			     list.push(gym.getAttribute("href").replace("/lectio/"+ID+"/SkemaNy.aspx?type=elev&elevid=", "")+"=="+gym.text);
+		        })
+            }
+        }
+    })
 }
 
 function search() {
@@ -33,7 +53,6 @@ function search() {
             var patt = new RegExp(search, "i");
             if (patt.test(current[1])) { //calls on match
                 IDs.push(current); //sets the ID of the match into StringArray
-                //console.log(current);
             }
         }
     }
